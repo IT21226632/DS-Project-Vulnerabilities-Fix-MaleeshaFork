a388bd8636c61282d757338cbfc9d4814622c4d7
IT21226632 <it21226632@my.sliit.lk>
2024-09-23 16:41:17 +0530

    fix missing key issue

--- frontend/src/components/instructor/view-course/test.js
+++ frontend/src/components/instructor/view-course/test.js
@@ -119,8 +119,8 @@ function ViewCourse() {
                     {courseContents.map((content, index) => (
                        <div className='view-content-list'>
                        
-                            <div className='view-content-list-item' >
-                                <div className='view-content-list-item-image' key={index}>
+                            <div className='view-content-list-item' key={content.id || index} >
+                                <div className='view-content-list-item-image' >
                                 <iframe
                                     style={{ width: '100%', height: '100%' }}
                                     src={`https://www.youtube.com/embed/${getYouTubeVideoId(content.lectureVideo)}`}
